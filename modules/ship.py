341c7c05b0025c464e8533aacef19e630c7be6fd
Joao Costa <joao.costa@ARYZTA.COM>
2019-04-22 13:45:48 -0300

    Draft inicial da classe Ship

---
+++ modules/ship.py
@@ -0,0 +1,23 @@
+class Ship():
+    def __init__(self, pos_x, pos_y):
+        self.accel = 0
+        self.y_vel = 0
+        self.x_vel = 0
+        self.pos_x = pos_x
+        self.pos_y = pos_y
+
+    def move_up():
+        self.y_vel = y_vel - self.accel
+        self.pos_y = self.pos_y - y_vel
+
+    def move_down():
+        self.y_vel = y_vel + self.accel
+        self.pos_y = self.pos_y + y_vel
+
+    def move_left():
+        self.x_vel = x_vel - self.accel
+        self.pos_x = self.pos_x - x_vel
+
+    def move_right():
+        self.x_vel = x_vel + self.accel
+        self.pos_x = self.pos_y + x_vel
