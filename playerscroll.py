131343c289d0d796abe136bbf4929434103ef9e9
Joao Costa <joao.costa@ARYZTA.COM>
2019-04-22 13:13:10 -0300

    implementação de tiro feita pelo tiozão

--- playerscroll.py
+++ playerscroll.py
@@ -1,5 +1,6 @@
 import math, random, sys
 import pygame
+import time
 from pygame.locals import *
 
 # exit the program
@@ -10,7 +11,7 @@ def events():
                 sys.exit()
 
 # define display surface            
-W, H = 1280, 720
+W, H = 1280, 650
 HW, HH = W / 2, H / 2
 AREA = W * H
 
@@ -30,7 +31,6 @@ GREEN = (0, 255, 0)
 # define player score system
 score = 0
 
-
 # define lifebar
 life = 100
 damage = 100
@@ -40,14 +40,12 @@ bg = pygame.image.load("mountains.png").convert()
 spaceship = pygame.transform.scale(pygame.image.load("spaceship.png").convert(), (50, 50))
 bgWidth, bgHeight = bg.get_rect().size
 
-
 # define level size. For increase level difficult, change the factor after bgWidth
 stageWidth = bgWidth * 2
 stagePosX = 0
 
 # define inicial scroll status
 startScrollingPosX = HW
-
 
 circleRadius = 25
 spaceShipPosX = circleRadius
@@ -57,6 +55,17 @@ playerPosY = H / 2
 playerVelocityX = 0
 playerVelocityY = 0
 
+#definindo um array com os tiros do jogador
+tiros = []
+
+#Função para desenhar todos os tiros
+def desenharTiro(tiros):
+    for t in range(len(tiros)):
+        if(tiros[t]['status'] == 1):
+            pygame.draw.rect(DS, RED, pygame.Rect(tiros[t]['x'], tiros[t]['y'], 10, 5))
+
+            tiros[t]['x'] += 10    
+
 # main loop
 while True:
     events()
@@ -64,19 +73,21 @@ while True:
     k = pygame.key.get_pressed()
     
     if k[K_RIGHT]:
+        playerPosX += 1
         playerVelocityX = 1
-        if life >= 0 and damage <= 100:
-                life -= 1       
+        
     elif k[K_LEFT]:
+        playerPosX -= 1
         playerVelocityX = -1
-        if life <= 100 and damage >= 0:
-                life += 1
+        
     elif k[K_UP]:
         playerPosY -= 1
 
     elif k[K_DOWN]:
         playerPosY += 1
-    
+
+    elif k[K_SPACE]:
+        tiros.append({'x': playerPosX+50, 'y': playerPosY+23, 'status': 1})
     else:
         playerVelocityX = 0
         
@@ -97,6 +108,9 @@ while True:
     pygame.draw.rect(DS, RED, pygame.Rect(20, 20, 100, 30))
     pygame.draw.rect(DS, GREEN, pygame.Rect(20, 20, life, 30))
     DS.blit(spaceship, (int(spaceShipPosX), int(playerPosY)))
+
+    
+    desenharTiro(tiros)
 
 
     pygame.display.update()
