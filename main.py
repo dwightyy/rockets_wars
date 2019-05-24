fa887410adb3ada5dcbdbc415bfe2b971ed062d8
Gustavo Sousa Barros <32529223+Gustavo59@users.noreply.github.com>
2019-05-24 20:38:42 -0300

    Tempo, Score e Trocando BackGround
    Criei a lógica para contar o tempo em segundos e minutos, assim como para contar os pontos assim que uma nave é atingida. Para trocar o fundo eu fiz lógica para a partir de um certo tempo alterar o fundo e zerar o tempo. Mas tem muito o que melhorar ainda.

--- main.py
+++ main.py
@@ -17,17 +17,26 @@ CLOCK = pygame.time.Clock()
 DS = pygame.display.set_mode((W, H))
 pygame.display.set_caption("Enemies Atacks the moon - seasson 1")
 
+#score
+score = 0
+
+#defining time
+minutes = 0
+seconds = 0
+milliseconds = 0
 
 # define lifebar
 life = 100
 damage = 100
 
+#reference to fase
+fase = 1
+
 # create background and main character images
-bg = pygame.image.load("assets/imgs/winter.jpg").convert()
+bg = pygame.image.load("assets/imgs/forest.jpg").convert()
 bg_rect = bg.get_rect()
 
 # main loop
-
 
 all_sprites = pygame.sprite.Group()
 bullets = pygame.sprite.Group()
@@ -38,7 +47,7 @@ all_sprites.add(ship)
 x = 0
 
 def spawn_enemy_ships(enemy_ships):
-    for i in range(3):
+    for i in range(5):
         random_y, random_x = random.randint(50, 680), random.randint(1000, 1150)
         enemy_ships.add(EnemyShip(random_x, random_y))
     return enemy_ships
@@ -48,12 +57,11 @@ enemy_ships = spawn_enemy_ships(enemy_ships)
 all_sprites.add(enemy_ships)
 start_ticks=pygame.time.get_ticks()
 
-
 while True:
-    CLOCK.tick(FPS)
-    seconds=(pygame.time.get_ticks()-start_ticks)/1000
-    game_time = 
-    game_fase = FONT.render("Fase 1", False, (BLACK))
+    CLOCK.tick(FPS)    
+    game_fase = FONT.render("Fase " + str(fase), False, (WHITE))
+    
+    
     for event in pygame.event.get():
         if event.type == QUIT or (event.type == KEYDOWN and (event.key == K_ESCAPE or event.key == K_q)):
             pygame.quit()
@@ -63,10 +71,11 @@ while True:
                 all_sprites = ship.shoot(all_sprites, bullets)
                 print(all_sprites, bullets)
 
-        #teste
-        elif CLOCK.tick(FPS)
-        seconds=(pygame.time.get_ticks()-start_ticks)/1000
-        game_fase = FONT.render('Fase 1', False, (BLACK) event in pyga)
+    seconds = int((pygame.time.get_ticks()-start_ticks)/1000)
+    if seconds > 60:
+        minutes += 1
+        seconds = 0
+    timelabel = FONT.render("{}:{}".format(minutes, int(seconds)), False, (WHITE))    
     
 
     DS.fill(BLACK)
@@ -85,7 +94,32 @@ while True:
     all_sprites.update()
  
 
-    hits_enemy_ship = pygame.sprite.groupcollide(enemy_ships, bullets, True, True)
+    hits_enemy_ship = pygame.sprite.groupcollide(enemy_ships, bullets, True, True)    
+
+    if hits_enemy_ship != {} and fase == 1:
+        score += 10
+    if hits_enemy_ship != {} and fase == 2:
+        score += 20
+    if hits_enemy_ship != {} and fase == 3:
+        score += 30
+        
+    game_score = FONT.render("Score: " + str(score), False, (WHITE))
+
+    if minutes == 1 and seconds == 30 and fase == 1:
+        bg = pygame.image.load("assets/imgs/mountains.png").convert()
+        bg_rect = bg.get_rect()
+        start_ticks=pygame.time.get_ticks()
+        seconds = 0
+        minutes = 0
+        fase = 2
+
+    if minutes == 2 and seconds == 30 and fase == 2:
+        bg = pygame.image.load("assets/imgs/space.jpg").convert()
+        bg_rect = bg.get_rect()
+        start_ticks=pygame.time.get_ticks()
+        seconds = 0
+        minutes = 0
+        fase = 3    
         
 
     #pygame.draw.rect(DS, RED, pygame.Rect(20, 20, 100, 30))
@@ -93,8 +127,10 @@ while True:
 
    # Draw / render
 
-    DS.blit(game_time, (10, 0))
-    DS.blit(game_fase, (10, 40))
+    DS.blit(game_fase, (10, 20))
+    DS.blit(timelabel,(1180, 20))
+    DS.blit(game_score, (10, 60))
+    
     all_sprites.draw(DS)
     # *after* drawing everything, flip the display
     pygame.display.flip()
